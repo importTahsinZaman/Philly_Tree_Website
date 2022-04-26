bc4c5fd43d5fc082de4139f9dd085ba9f70b5e7a
importTahsinZaman <tahsinz21366@gmail.com>
2022-04-26 09:11:26 -0400

    compressed video files

--- Scripts/tree_directory.js
+++ Scripts/tree_directory.js
@@ -8,17 +8,3 @@ var color_pick = 0;
 document.addEventListener("mousemove", (e) => {
   mouse_counter += 1;
 });
-
-// function animate() {
-//   if (mouse_counter >= 10) {
-//     mouse_counter = 0;
-
-//     color_pick = Math.floor(Math.random() * text_colors.length);
-
-//     r.style.setProperty("--background-color", background_colors[color_pick]);
-//     r.style.setProperty("--text-color", text_colors[color_pick]);
-//   }
-//   setTimeout(animate, 250);
-// }
-
-// animate();
