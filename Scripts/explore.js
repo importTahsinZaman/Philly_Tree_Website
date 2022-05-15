01f4d1fedba9da89c0b653abc8bc9f1026f35cbb
importTahsinZaman <tahsinz21366@gmail.com>
2022-05-14 22:13:34 -0400

    Added trees to directory

--- Scripts/explore.js
+++ Scripts/explore.js
@@ -1,4 +1,11 @@
+//Skrollr Initialization
 var s = skrollr.init();
+
+//Intro slider
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.1 });
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
 
 //Intro menu
 $(function () {
